e1a018cf7d670f24098d1daa1386801c0b50f9e7
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-03-09 02:13:02 -0500

    1

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NewNotesPage from '../NewNotesPage/NewNotesPage';
+import NotesHistoryPage from '../ONotesHistoryPage/NotesHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
@@ -17,8 +17,8 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="/notes" element={<NotesHistoryPage notes={notes} user={user} setNotes={setNotes}/>} />
+              <Route path="/notes/new" element={<NewNotesPage setNotes={setNotes} notes={notes} user={user} />} />
             </Routes>
           </>
           :
