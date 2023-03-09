c5dc01def5b8bebf9a777eaa6881aa8e5e5f5d74
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-03-09 02:15:47 -0500

    app.js done

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -17,8 +17,8 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/notes" element={<NotesHistoryPage notes={notes} user={user} setNotes={setNotes}/>} />
-              <Route path="/notes/new" element={<NewNotesPage setNotes={setNotes} notes={notes} user={user} />} />
+              <Route path="/notes" element={<NotesHistoryPage user={user} setUser={setUser}/>} />
+              <Route path="/notes/new" element={<NewNotesPage user={user} setUser={setUser} />} />
             </Routes>
           </>
           :
