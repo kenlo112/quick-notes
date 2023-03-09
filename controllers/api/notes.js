4feea0bde2d8b905b17fdede81a5dcb934101c83
Tin Ian Lo <lotinian@Tindebijixingdiannao.local>
2023-03-09 02:02:50 -0500

    controller notes

---
+++ controllers/api/notes.js
@@ -0,0 +1,23 @@
+const Note = require("../../models/note");
+
+module.exports = {
+    create,
+      index,
+  };
+  
+  async function index(req, res) {
+    const notes = await Note.find({});
+    res.status(200).json(notes);
+  }
+  
+  async function create(req, res) {
+    try {
+      console.log(req.body);
+      req.body.user = req.user._id;
+      const note = await Note.create(req.body);
+      res.status(200).json(note);
+    } catch (err) {
+      res.status(500).json(err);
+      console.log(err);
+    }
+  }
